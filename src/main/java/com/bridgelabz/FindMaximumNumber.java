450d1090c63d34c7dbc2cbd5e37d66e01143e8e6
pravindeshmukh1 <pravin2391@gmail.com>
2020-03-27 11:31:45 +0530

    ADDED: Welcome Message

--- src/main/java/com/bridgelabz/FindMaximumNumber.java
+++ src/main/java/com/bridgelabz/FindMaximumNumber.java
@@ -1,4 +1,7 @@
 package com.bridgelabz;
 
 public class FindMaximumNumber {
+    public static void main(String[] args) {
+        System.out.println("Welcome Find Maximum Number Problem");
+    }
 }
