2f0e5a40534fd0a5d27d8e37124f60584ffa1c66
pravindeshmukh1 <pravin2391@gmail.com>
2020-03-28 16:07:09 +0530

    ADDED: Function to find Maximum Integer Number at second position

--- src/test/java/MaximumNumberTest.java
+++ src/test/java/MaximumNumberTest.java
@@ -7,9 +7,17 @@ public class MaximumNumberTest {
 
     // Integer Number
     @Test
-    public void givenFirstNumber_ifMaximumNumber_shouldReturnSameNo() {
+    public void givenThreeIntegerNumber_findMaximumNumber_shouldReturnSameNo() {
         MaximumNumber maximumNumber = new MaximumNumber();
         int maxNum = maximumNumber.findMaxValue(400, 100, 200);
         Assert.assertEquals(400, maxNum);
     }
+
+    @Test
+    public void givenThreeIntegerNumber_findMaximumNumber_shouldReturnSecondNo() {
+        MaximumNumber maximumNumber = new MaximumNumber();
+        int maxNum = maximumNumber.findMaxValue(100, 400, 200);
+        Assert.assertEquals(400, maxNum);
+    }
+    
 }
