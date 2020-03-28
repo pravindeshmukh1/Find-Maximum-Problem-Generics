37d96917eb2f206f05c1cb0c30288cb82f13f083
pravindeshmukh1 <pravin2391@gmail.com>
2020-03-28 17:05:10 +0530

    ADDED: Function to find Maximum Float Number at second position return second number

--- src/test/java/MaximumNumberTest.java
+++ src/test/java/MaximumNumberTest.java
@@ -2,9 +2,6 @@ import com.bridgelabz.MaximumNumber;
 import org.junit.Assert;
 import org.junit.Test;
 
-import static org.junit.Assert.assertEquals;
-
-
 public class MaximumNumberTest {
 
     // Integer Number
@@ -28,13 +25,19 @@ public class MaximumNumberTest {
         int maxNum = maximumNumber.findMaxIntegerValue(100, 200, 400);
         Assert.assertEquals(400, maxNum);
     }
+
     // Float Number
     @Test
     public void givenThreeFloatNumber_findMaximumNumber_shouldReturnSameNo() {
         MaximumNumber maximumNumber = new MaximumNumber();
-        Float maxFloatValue= maximumNumber.findMaxFloatValue(40.40f, 10.10f, 20.20f);
-        System.out.println(maxFloatValue);
-        Assert.assertEquals((Float)40.40f, maxFloatValue);
+        Float maxFloatValue = maximumNumber.findMaxFloatValue(40.40f, 10.10f, 20.20f);
+        Assert.assertEquals((Float) 40.40f, maxFloatValue);
+    }
+    @Test
+    public void givenThreeFloatNumber_findMaximumNumber_shouldReturnSecondNo() {
+        MaximumNumber maximumNumber = new MaximumNumber();
+        Float maxFloatValue = maximumNumber.findMaxFloatValue(10.10f, 40.40f, 20.20f);
+        Assert.assertEquals((Float) 40.40f, maxFloatValue);
     }
 
 }
